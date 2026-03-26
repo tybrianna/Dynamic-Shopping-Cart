d9a0d5b53ca300cce8bef3572aae5ebaa1e7bb74
tybrianna <tybriannahall@gmail.com>
2026-03-26 09:39:06 -0400

    added function to the add product button

--- scripts.js
+++ scripts.js
@@ -19,3 +19,31 @@ function removeItem(event) {
   updateTotalPrice(-price);
   item.remove();
 }
+
+// Function to add product to list
+addProductButton.addEventListener("click", () => {
+    const name = productNameInput.value;
+    const price = parseFloat(productPriceInput.value);
+  if (!name || isNaN(price)) {
+    alert("Enter product name to add to list");
+    return;
+  }
+
+//   
+  const li = document.createElement("li");
+  li.dataset.price = price;
+
+  li.innerHTML = `
+    ${name} - $${price.toFixed(2)}
+    <button>Remove</button>
+  `;
+
+  li.querySelector("button").addEventListener("click", removeItem);
+
+  cart.appendChild(li);
+
+  updateTotalPrice(price);
+
+  productNameInput.value = "";
+  productPriceInput.value = "";
+});
